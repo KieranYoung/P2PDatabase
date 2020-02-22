4b31ab84f0227cdab0947228013c873ec5d16248
Alexander Larkin <aplarkin@mtu.edu>
2020-02-22 12:29:29 -0500

    Added notification to manifest

---
+++ app/src/main/java/com/example/p2pdatabase/com/example/p2pdatabase/services/NotificationService.java
@@ -0,0 +1,18 @@
+package com.example.p2pdatabase.com.example.p2pdatabase.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+public class NotificationService extends IntentService {
+
+    public NotificationService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
