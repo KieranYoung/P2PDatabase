117bf894806484051600779444077821633d9840
Alexander Larkin <aplarkin@mtu.edu>
2020-02-22 12:35:48 -0500

    Nearby Services for Big Brain of Big Brains

---
+++ app/src/main/java/com/example/p2pdatabase/com/example/p2pdatabase/services/NearbyServiceReceive.java
@@ -0,0 +1,17 @@
+package com.example.p2pdatabase.com.example.p2pdatabase.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+public class NearbyServiceReceive extends IntentService {
+    public NearbyServiceReceive(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/com/example/p2pdatabase/com/example/p2pdatabase/services/NearbyServiceSend.java
@@ -0,0 +1,19 @@
+package com.example.p2pdatabase.com.example.p2pdatabase.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+public class NearbyServiceSend extends IntentService {
+
+    public NearbyServiceSend(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+
+}
